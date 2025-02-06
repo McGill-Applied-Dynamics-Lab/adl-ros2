480a0a72c51149f4687c645a1166c7bce10cf684
Charles <33535609+Syoris@users.noreply.github.com>
2025-02-06 16:00:41 -0500

    Folders refactoring (#9)
    * folder refactoring
    
    * readme
    
    * refactoring robot_arm -> robot_arm_client

---
+++ robot_arm/README.md
@@ -0,0 +1,11 @@
+# robot_arm
+Collection of ros2 packages to control robotic arms using python.
+
+Supported arms:
+- Franka Research 3
+- Vectis?
+
+
+## Packages
+
+## Installation
--- robot_arm/robot_arm_client/scripts/robot_arm_test.py
+++ robot_arm/robot_arm_client/scripts/robot_arm_test.py
@@ -1,5 +1,5 @@
 import rclpy
-from robot_arm.FrankaArm import FrankaArm
+from robot_arm_client.FrankaArm import FrankaArm
 from rclpy.duration import Duration
 
 import numpy as np
@@ -19,19 +19,19 @@ def main(args=None):
     # # Move joints home
     # franka_arm.goto_home()
 
-    # # Move to pose
-    # pose_goal = SE3(0.5, 0.5, 0.5)
-    # franka_arm.goto_pose(pose_goal, 5.0)
+    # Move to pose
+    pose_goal = SE3(0.5, 0.5, 0.5)
+    franka_arm.goto_pose(pose_goal, 5.0)
 
     # # Joint velocities
     # joint_vels = [40, 0, 0, 0, 0, 0, 0]
     # duration = 5.0
     # franka_arm.goto_joint_vels(joint_vels, unit="deg", duration=Duration(seconds=duration))
 
-    # EE velocities
-    ee_vel = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    duration = 2.0
-    franka_arm.goto_ee_vel(ee_vel, duration=Duration(seconds=duration))
+    # # EE velocities
+    # ee_vel = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    # duration = 2.0
+    # franka_arm.goto_ee_vel(ee_vel, duration=Duration(seconds=duration))
 
     # Clean
     franka_arm.destroy_node()
