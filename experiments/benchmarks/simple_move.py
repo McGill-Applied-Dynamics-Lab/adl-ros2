487dfdb5f8bd086e5dfd831a2bab0a1771759c2f
Wilfred-Mason <wilfred.mason@mail.mcgill.ca>
2025-10-29 15:17:17 -0400

    probing working

--- experiments/benchmarks/simple_move.py
+++ experiments/benchmarks/simple_move.py
@@ -13,27 +13,24 @@ robot.wait_until_ready()
 
 # %%
 print(robot.end_effector_pose)
-print(robot.joint_values)
 print(robot.end_effector_wrench)  # added to print initial wrench
 
-robot.controller_switcher_client.switch_controller("haptic_controller")
-robot.haptic_controller_parameters_client.load_param_config(
-    file_path=CONFIG_DIR / "controllers" / "probe_controller.yaml"
+robot.controller_switcher_client.switch_controller("joint_space_controller")
+robot.joint_space_controller_parameters_client.load_param_config(
+    file_path=CONFIG_DIR / "controllers" / "joint_space_controller.yaml"
 )
 
 
 print("Moving to start position...")
-start_position = np.array([0.615, -0.0714, 0.331 + 0.05]) # 0.331
-start_position = np.array([0.550, -0.0714, 0.35])  # button location
-
-# inc = np.array([-0.05,-0.05,0])
-# start_position = start_position + inc
+start_position = np.array([0.50, -0.10, 0.40])
 robot.move_to(position=start_position, speed=0.05)
 time.sleep(1.0)
+
 target_pose = robot.end_effector_pose.copy()
 target_pose.orientation = R.from_euler("xyz", [-180, 0, 0], degrees=True)  # roll, pitch, yaw
 robot.set_target(pose=target_pose)
 time.sleep(1.0)
+
 """
 for step in np.arange(0,50,5):
     target_pose.orientation = R.from_euler("xyz", [-180, 0, 0], degrees=True)  # roll, pitch, yaw
