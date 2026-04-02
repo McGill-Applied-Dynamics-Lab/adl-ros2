6186fa3372a0fe545c9fd4be1cf46efb12366569
shiyao <shiyao.ni@mail.mcgill.ca>
2026-04-02 19:55:23 -0400

    Vendor franka_ros2 and document franka_msgs setup

--- examples/11_open_gripper.py
+++ examples/11_open_gripper.py
@@ -1,13 +1,13 @@
 """Open the FR3 gripper fully."""
 
 from arm_client.robot import Robot
-from arm_client.gripper.franka_hand import FrankaHand
+from arm_client.gripper.franka_hand import Gripper
 
 
 robot = Robot(namespace="fr3")
 robot.wait_until_ready(timeout=2.0)
 
-gripper = FrankaHand(node=robot.node, namespace="fr3")
+gripper = Gripper(node=robot.node, namespace="fr3")
 gripper.wait_until_ready(timeout=5.0)
 
 print("Opening gripper to max width...")
