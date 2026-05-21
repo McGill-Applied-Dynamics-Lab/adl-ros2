632a5f8c78b49a9201ca920b182eab5eb06cf53a
Charles Sirois <charles.sirois@mail.mcgill.ca>
2026-05-21 16:54:30 -0400

    wip: experiment manager

--- python/rim_teleop/src/rim_teleop/adapters/__init__.py
+++ python/rim_teleop/src/rim_teleop/adapters/__init__.py
@@ -1,6 +1,6 @@
 """ROS/device adapter layer for RIM teleoperation."""
 
-from .experiment_logger_adapter import ExperimentLogger
+from ..data.logger import ExperimentLogger
 from .model_estimator_adapter import RobotModelAdapter
 from .teleop_interface_adapter import TeleopInterfaceAdapter
 
